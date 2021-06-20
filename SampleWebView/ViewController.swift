4ae1893a5f393396e11327c73b4571cf5f2e224f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-06-20 15:30:34 -0700

    2021_06_20

--- SampleWebView/ViewController.swift
+++ SampleWebView/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import WebKit //WebView 사용을 위한 import
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WKUIDelegate, WKNavigationDelegate {
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 1. url string
+        // 2. url -> request
+        // 3. req -> load
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString){
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+        
     }
-
 
 }
 
